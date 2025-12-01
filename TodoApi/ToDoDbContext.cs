fd73cd8a27464f215aa49f81f85a316433e6b657
“YehuditRayman” <y0556741787@gmail.com>
2025-12-02 00:12:15 +0200

    p4-1

--- TodoApi/ToDoDbContext.cs
+++ TodoApi/ToDoDbContext.cs
@@ -19,7 +19,7 @@ public partial class ToDoDbContext : DbContext
     public virtual DbSet<Item> Items { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        => optionsBuilder.UseMySql("name=ToDoDB", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.44-mysql"));
+        => optionsBuilder.UseMySql("name=bc4qgcvk7cvcdpararfm", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.44-mysql"));
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
